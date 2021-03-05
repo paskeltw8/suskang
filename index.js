7693a817c21e5adeddfe969d3068c337ef7106d1
yaxley-peaks <epiclycoolgaemer@gmail.com>
2021-03-05 19:49:13 +0530

    handled when bot is muted

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const sokang = "513808991147655168";
 const Discord = require("discord.js");
 const dotenv = require("dotenv");
+var flag = 0;
 dotenv.config();
 
 const client = new Discord.Client();
@@ -8,17 +9,31 @@ const client = new Discord.Client();
 client.once("ready", () => console.log("sus"));
 
 client.on("message", async (msg) => {
-    if (msg.author.id == sokang) {
-        try {
-            const suskang = msg.guild
-                .emojis
-                .cache
-                .find(emoji => emoji.name === "suskang");
-            await msg.react(suskang);
-        } catch { 
-            msg.channel.send("something went wrong 😔\nmaybe :suskan: doesnt exist");
+    try {
+        if (msg.author.id == sokang) {
+            try {
+                const suskang = msg.guild
+                    .emojis
+                    .cache
+                    .find(emoji => emoji.name === "suskang");
+                await msg.react(suskang);
+            } catch {
+                msg.channel.send("something went wrong 😔\nmaybe :suskan: doesnt exist");
+            }
         }
+    } catch {
+        if (!flag)
+            flag != fag;
     }
 });
 
 client.login(process.env.SECRET);
+
+function buildErrorsAndSend(channel) {
+    if (flag) {
+        channel.send("YOU ABSOLUTE RETARD HOW DARE MUTE ME!!!!!!!!!!!!!!!!!");
+        flag != flag;
+        return;
+    }
+    return;
+}
