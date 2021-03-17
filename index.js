0d7283318bcb013ce721a3d53d770525b90a56b1
yaxley-peaks <epiclycoolgaemer@gmail.com>
2021-03-17 19:00:09 +0530

    added ping list

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const sokang = "513808991147655168";
 const discord = require("discord.js");
+const targets = require("./list.json").targets;
+const sokang = "513808991147655168";
 var flag = 0;
 var self;
-
+const guildId = "689964611004399673" //orion server
 const client = new discord.Client();
 
-client.once("ready", () => console.log("sus"));
+client.once("ready", async () => {
+    console.log("sus")
+    // await buildMemberList();
+});
 
 client.on("message", async (msg) => {
-    ping(msg.channel, ["513808991147655168"]); //add more members
+    ping(msg.channel, targets, msg.author.id); //add more members
     self = msg.guild.member(client.user);
     try {
         buildErrorsAndSend(msg.channel);
@@ -34,9 +38,6 @@ client.on('messageUpdate', async (msg) => {
     if (msg.author.bot) return;
     await msg.channel.send(`lol <@${msg.author.id}> is a retard who cant even spell`);
 });
-
-
-// client.login(process.env.SECRET);
 client.login(process.env.SECRET);
 
 
@@ -59,12 +60,21 @@ function sendLRandomly(channel) {
 }
 
 //pings one of the targets randomly if message not sent by author
-function ping(channel, targets, author = "815186501632786449") {
+function ping(channel, targets, author) {
     let now = Date.now();
     if (now % 77 == 0) {
-        if (author == "815186501632786449")
+        if (author == "815186501632786449") //itself
             return;
         let cur = targets[Math.floor(Math.random() * targets.length)];
         channel.send(`<@${cur}> hah get pinged`);
     }
+}
+
+async function buildMemberList() {
+    const member = await client.guilds.fetch(guildId);
+    console.log("got server");
+    const list = await member.members.fetch()
+    console.log("getting members");
+    console.log(list);
+
 }
